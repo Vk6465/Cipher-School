30babc6842499f0d636b5c2cd0af8c5dd3fe0cf8
Vinod Kumar Reddy Gorantla <104248330+Vk6465@users.noreply.github.com>
2023-06-16 14:53:45 +0530

    Add files via upload

---
+++ lecture4.1-CipherSchool.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //declare 4 integers
+    int a,b,c,d;
+    cout<<"Input 4 integers a, b, c,and d";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    // Statement
+    cout<<(a>b && c>d);
+    //C++ intrtprets true as 1 and false as 0 
+}
---
+++ lecture4.2-CipherSchool.cpp
@@ -0,0 +1,105 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Give 2 floating point numbers as input"<<endl;
+    cin>>a>>b;
+
+    if(a>0.01 && b>a){
+        cout<<"The condition in if statement is correct";
+    }
+    else
+    {
+        cout<<"The condition is false";
+    }
+}
+*/
+
+
+// if usuage
+
+/*
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Student, please enter the marks to calculate the grade : ";
+    cin>>marks;
+
+    if(marks>90)
+    {
+        cout<<"A";
+    }
+    if(marks<90 && marks>80)
+    {
+        cout<<"B";
+    }
+    if(marks<80 && marks>70)
+    {
+        cout<<"C";
+    }
+    if(marks<70)
+    {
+        cout<<"Pass";
+    }
+}
+*/
+
+
+/*
+
+
+//else if usuage
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"Student, please enter the marks to calculate the grade : ";
+    cin>>marks;
+
+    if(marks>90)
+    {
+        cout<<"A";
+    }
+    else if(marks>80)
+    {
+        cout<<"B";
+    }
+    else if(marks>70)
+    {
+        cout<<"C";
+    }
+    else
+    {
+        cout<<"Pass";
+    }
+}
+
+
+
+*/
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    cout<<"give 4 integers as input :a,b,c,d"<<endl;
+    cin>>a>>b>>c>>d;
+    if(a>b && c>d){
+        cout<<"HAHA";
+    }
+    else if(a<b && c>d){
+        cout<<"HEHE";
+    }
+    else {
+        cout<<"HUHU";
+    }
+
+}
+
+
